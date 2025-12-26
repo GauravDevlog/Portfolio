536513d4f57a98c855f82188ac56b59500abc933
gauravmishra00 <gauravonbeat@gmail.com>
2025-12-26 21:22:55 +0530

    Some Netlify Fix

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
       <div className="home_design">
         <div className="profile1">
           <img
-            src="../../public/img/gauravImg.jpeg"
+            src="img/gauravImg.jpeg"
             alt="Profile Img"
             className="profile"
           />
